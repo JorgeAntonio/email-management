445164a2e7be8339c2feb80566c436fe7cac8a93
unknown <UNAP@economicoslap05.postgradounap.edu.pe>
2026-02-19 13:00:39 -0500

    fixed data

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,9 @@
+import { ResetPasswordForm } from './reset-password-form';
+
+export default function ResetPasswordPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#F5F7FA] p-4">
+      <ResetPasswordForm />
+    </div>
+  );
+}
